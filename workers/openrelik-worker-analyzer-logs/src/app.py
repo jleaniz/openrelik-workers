c0d1a3005b41c3443b345d43bacf44ec170ba174
hacktobeer <ramsesdebeer@gmail.com>
2026-02-12 14:50:11 +0100

    Fix celery logger hijacking. (#26)

--- workers/openrelik-worker-analyzer-logs/src/app.py
+++ workers/openrelik-worker-analyzer-logs/src/app.py
@@ -20,13 +20,20 @@ from celery.app import Celery
 from openrelik_common import telemetry
 from openrelik_worker_common.debug_utils import start_debugger
 
-telemetry.setup_telemetry('openrelik-worker-analyzer-logs')
+telemetry.setup_telemetry("openrelik-worker-analyzer-logs")
 
 if os.getenv("OPENRELIK_PYDEBUG") == "1":
     start_debugger()
 
 REDIS_URL = os.getenv("REDIS_URL") or "redis://localhost:6379/0"
-celery = Celery(broker=REDIS_URL, backend=REDIS_URL, include=["src.tasks"])
+celery = Celery(
+    broker=REDIS_URL,
+    backend=REDIS_URL,
+    include=["src.tasks"],
+    worker_hijack_root_logger=False,  # Disable Celery hijacking configured Python loggers.
+    worker_log_format="%(message)s",
+    worker_task_log_format="%(message)s",
+)
 redis_client = redis.Redis.from_url(REDIS_URL)
 
 telemetry.instrument_celery_app(celery)
--- workers/openrelik-worker-os-creds/src/app.py
+++ workers/openrelik-worker-os-creds/src/app.py
@@ -20,15 +20,20 @@ from celery.app import Celery
 from openrelik_common import telemetry
 from openrelik_worker_common.debug_utils import start_debugger
 
-telemetry.setup_telemetry('openrelik-worker-os-creds')
+telemetry.setup_telemetry("openrelik-worker-os-creds")
 
 if os.getenv("OPENRELIK_PYDEBUG") == "1":
     start_debugger()
 
-REDIS_URL = os.getenv("REDIS_URL")
-celery = Celery(broker=REDIS_URL,
-                backend=REDIS_URL,
-                include=["src.linux_acct_task", "src.windows_acct_task"])
+REDIS_URL = os.getenv("REDIS_URL") or "redis://localhost:6379/0"
+celery = Celery(
+    broker=REDIS_URL,
+    backend=REDIS_URL,
+    include=["src.linux_acct_task", "src.windows_acct_task"],
+    worker_hijack_root_logger=False,  # Disable Celery hijacking configured Python loggers.
+    worker_log_format="%(message)s",
+    worker_task_log_format="%(message)s",
+)
 redis_client = redis.Redis.from_url(REDIS_URL)
 
 telemetry.instrument_celery_app(celery)
